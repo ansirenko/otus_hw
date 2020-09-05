b69a1369bb2a9625ddcc27b96b0ae842cdba2ee8
Aleksandr Sirenko <a.n.sirenko@yandex.ru>
2020-09-05 13:18:18 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	currentTime := time.Now().Round(0).UTC()
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("failed to get exact time")
+	}
+	fmt.Println("current time:", currentTime)
+	fmt.Println("exact time:", exactTime.UTC().Round(0))
 }
