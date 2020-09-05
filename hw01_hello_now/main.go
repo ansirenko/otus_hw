e5f9aff1dfb4049f94f91c53e5a96afc85ec52fd
Aleksandr Sirenko <a.n.sirenko@yandex.ru>
2020-09-05 17:13:46 +0300

    add variable with host (travis...)

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,11 +9,12 @@ import (
 )
 
 func main() {
-	currentTime := time.Now().Round(0).UTC()
-	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	currentTime := time.Now().UTC()
+	timeHost := "0.beevik-ntp.pool.ntp.org"
+	exactTime, err := ntp.Time(timeHost)
 	if err != nil {
-		log.Fatalf("failed to get exact time")
+		log.Fatalf("failed to get exact time from host: %s", timeHost)
 	}
-	fmt.Println("current time:", currentTime)
+	fmt.Println("current time:", currentTime.Round(0))
 	fmt.Println("exact time:", exactTime.UTC().Round(0))
 }
