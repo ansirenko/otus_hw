b9c3cea543a5f65728ded4496d450345ae724cfc
Aleksandr <a.n.sirenko@mail.ru>
2020-12-02 00:37:59 +0300

    terminate func and new tests

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	if len(stages) == 1 {
+		return in
+	}
+	out := in
+	for _, stage := range stages {
+		out = stage(terminator(done, out))
+	}
+	return out
+}
+
+func terminator(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case i, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- i
+			}
+		}
+	}()
+	return out
 }
